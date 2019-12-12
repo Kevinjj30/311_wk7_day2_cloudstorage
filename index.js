2bd6f179f08e89dc0d4ec2ec91e6d93acb7a7fc1
kevinjj30 <kevinjenkins56@yahoo.com>
2019-12-11 19:59:05 -0600

    xtra credit

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 const {Storage} = require('@google-cloud/storage');
 
 const storage = new Storage({
-  projectId: 'aca-311',
-  keyFilename: './aca-311-d864d06c299f.json'
+  projectId: 'expanded-torus-258100',
+  keyFilename: './My First Project-9bf91f53bfbb.json'
 })
 
 
-storage.bucket("aca-311-files").upload("./bruce.jpg", {
+storage.bucket("kevin-portfolio").upload("./bruce.jpg", {
     gzip: true,
     metadata: {
         cacheControl: 'public, max-age=31536000',
